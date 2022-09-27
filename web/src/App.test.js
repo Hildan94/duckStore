ebcea8a64e7f0909e84d9e82f10f1785d035ebfc
Hildibjørg <s164539@student.dtu.dk>
2022-09-27 11:22:46 +0200

    Initial commit

---
+++ web/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Login button exists',() =>{
+  render(<App/>);
+  let button = screen.getByRole("button");
+  expect(button).toHaveTextContent("Login");
+});
