41cd14ad9e41bf4c53b0b544b9f8d91a8b77599b
Hildibjørg <s164539@student.dtu.dk>
2022-09-27 12:10:02 +0200

    Added a greeting and change-name function

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [username, setUsername] = useState("Hildy")
+
   return (
     <div className="App">
-      <h3>Learn React with DevOps</h3>
-      <button>Login</button>
+      <h1>Learn React with DevOps</h1>
+        <div>Hello, {username}</div>
+
+        <button onClick={ (e)=> setUsername("Troels")}>Skift navn</button>
 
     </div>
   );
