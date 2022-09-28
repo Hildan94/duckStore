e04bff78178da060ed81d1ecaf0514889402b3df
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-09-28 19:41:58 +0200

    Changed the router back to be a Browserouter

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 //import the roboto font:
 import '@fontsource/roboto/300.css';
@@ -14,9 +14,9 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
