e9113afc6dd7a7736462380f4d272b635c2496a8
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-09-28 21:57:02 +0200

    deleted unused test

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -4,8 +4,4 @@ import {BrowserRouter} from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
 
-test('Testing the navigation to Books',() =>{
-  render(<BrowserRouter> <App/> </BrowserRouter>);
-  userEvent.click(screen.getByText(/Books/));
-  expect(screen.getByText(/Book list/)).toBeInTheDocument();
-});
+
