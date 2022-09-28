89724c7093990c5ab4e7e8e483a8cd23dad37d32
Hildibjørg Didriksen <s164539@student.dtu.dk>
2022-09-28 21:35:19 +0200

    Added a giraffe service with a GET rest method

---
+++ src/main/java/Hildy/GiraffeService.java
@@ -0,0 +1,20 @@
+package Hildy;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.*;
+
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("giraffes")
+
+public class GiraffeService {
+    List<String> giraffes = Arrays.asList("Melman", "Elmer");
+    @GET
+    public List<String> getGiraffes() {
+        return giraffes;
+    }
+}
